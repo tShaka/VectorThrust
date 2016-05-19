63623cda86ea7089f94ad720d0e87b1571cdc350
Michael Glasunov <michael.glasunov@stud.uni-bamberg.de>
2016-05-19 07:57:57 +0200

    initial attempt at datatypes

---
+++ src/Model.hs
@@ -0,0 +1,25 @@
+module Model
+( Velocity(..)
+, Position(..)
+, MassPoint(..)
+, Object(..)
+, ObjectType(..)
+, Level(..)
+) where
+
+-- TODO:
+-- make Velocity and Position instances of Vectorspace
+
+data Velocity = Velocity Double Double
+
+data Position = Position Float Float
+
+-- maybe incorporate radius
+data MassPoint = MassPoint Position Velocity
+
+data Object = Object MassPoint ObjectType
+data ObjectType = Player | Enemy
+-- alternatively instead of defining ObjectType?
+-- data Object = Player MassPoint | Enemy MassPoint
+
+data Level = Level [Object]
