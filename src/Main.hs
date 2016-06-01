1dd9d02848ea398212d3f25e641b26f4a3547123
Michael Schleier <michael-hermann.schleier@stud.uni-bamberg.de>
2016-06-01 16:29:36 +0200

    Added camera and keyboard control to fallingBall
    - added not moving circle to show movement
    - added moveable circle
    - added keyboard controls to move player
    - added camera that follows the player moving

--- src/Main.hs
+++ src/Main.hs
@@ -36,11 +36,15 @@ createAWindow title = do
 	-- set up IORefs
 	t <- getCurrentTime
 	timeRef <- newIORef t
+	-- saves position of player
+	playerPos <- newIORef (realToFrac 0, realToFrac 0)
 	-- set up ReactHandle
-	rh <- reactInit (initr) (actuate) (bouncingBall 5)
+	rh <- reactInit (initr) (actuate playerPos) (bouncingBall 5)
 	-- set up Callbacks
 	displayCallback $= display
 	idleCallback $=  Just (idle timeRef rh)
+	reshapeCallback $= Just resizeWindow
+	keyboardMouseCallback $= Just (keyboardMouse playerPos)
 	
 	return ()
 
@@ -53,9 +57,9 @@ initr :: IO ()
 initr = return ()
 
 --actuate react
-actuate :: ReactHandle () (Float, Float) -> Bool -> (Float, Float) -> IO Bool
-actuate _ _ (pos,vel) = do
-	renderBall pos
+actuate :: IORef (GLfloat,GLfloat) -> ReactHandle () (Float, Float) -> Bool -> (Float, Float) -> IO Bool
+actuate playerPos _ _ (pos,vel) = do
+	renderScene playerPos pos
 	return False
 	
 
@@ -73,6 +77,21 @@ idle timeRef rh = do
 	let dt = now `diffUTCTime` lastTime
 	react rh (realToFrac dt, Just ())
 	return ()
+	
+resizeWindow :: Size -> IO ()
+resizeWindow size = do
+	-- TODO keep aspect ratio
+	viewport $= (Graphics.UI.GLUT.Position 0 0, size)
+	return ()
+	
+keyboardMouse :: IORef(GLfloat,GLfloat) -> KeyboardMouseCallback
+keyboardMouse playerPos key Down _ _ = case key of
+	(SpecialKey KeyLeft) -> playerPos $~! \(x,y) -> (x-0.1,y)
+	(SpecialKey KeyRight) -> playerPos $~! \(x,y) -> (x+0.1,y)
+	(SpecialKey KeyUp) -> playerPos $~! \(x,y) -> (x,y+0.1)
+	(SpecialKey KeyDown) -> playerPos $~! \(x,y) -> (x,y-0.1)
+	_ -> return ()
+keyboardMouse _ _ _ _ _ = return ()
 	
 	
 ----------------- physics signal function ---------------------
@@ -120,16 +139,43 @@ player = createCircle 0.2
 translateTo :: Circle -> Model.Position -> Circle
 translateTo c pos = let (x'', y'') = convertToGL pos in map (\(x',y') -> (x'+x'', y'+y'')) c
 
+-- used to change the position of moveable circle
+translateTo' :: Circle -> (GLfloat,GLfloat) -> Circle
+translateTo' c (x,y) = map (\(x',y') -> (x'+x, y'+y)) c
+
 -- converts "normal" haskell variables into OpenGL variables
 convertToGL :: Model.Position -> (GLfloat, GLfloat)
 convertToGL (Model.Position x y) = (realToFrac x, realToFrac y)
-
+	
+-- draw moveable circle (=player)
+renderPlayer :: IORef (GLfloat,GLfloat) -> IO ()
+renderPlayer posRef = do
+		(x,y) <- readIORef posRef
+		repositionCamera (x,y)
+		renderPrimitive TriangleFan $ (color $ Color3 (1 :: GLfloat) (1 :: GLfloat) (1 :: GLfloat)) >> (mapM_ (\(x,y) -> vertex $ Vertex2 x y) (translateTo' player (x,y)))
+	
 -- draw the ball
 renderBall :: Float -> IO ()
 renderBall posY = do
-	clear [ColorBuffer]
+	-- draw not moving circle
+	renderPrimitive TriangleFan $ (color $ Color3 (1 :: GLfloat) (1 :: GLfloat) (0 :: GLfloat)) >> (mapM_ (\(x,y) -> vertex $ Vertex2 x y) (translateTo player (Model.Position (-0.5) 0)))
+	--draw falling ball
 	renderPrimitive TriangleFan $ (color $ Color3 (1 :: GLfloat) (1 :: GLfloat) (0 :: GLfloat)) >> (mapM_ (\(x,y) -> vertex $ Vertex2 x y) (translateTo player (Model.Position 0 posY)))
+	
+-- draw scene
+renderScene :: IORef (GLfloat, GLfloat) -> Float -> IO ()
+renderScene playerPos mBallPos = do
+	clear [ColorBuffer]
+	loadIdentity
+	renderPlayer playerPos
+	renderBall mBallPos
 	flush
 	
-
+-- position camera based on key commands
+repositionCamera :: (GLfloat, GLfloat) -> IO ()
+repositionCamera (x,y) = do
+	let camPos = Vertex3 (realToFrac x) (realToFrac y) (1::GLdouble)
+	let camView = Vertex3 (realToFrac x) (realToFrac y) (0::GLdouble)
+	let upVec = Vector3 (0::GLdouble) (1::GLdouble) (0::GLdouble)
+	lookAt camPos camView upVec
 
