1131abfcaa071c596b8fac25e842354aad4d022d
Michael Schleier <michael-hermann.schleier@stud.uni-bamberg.de>
2016-08-18 15:17:19 +0200

    Changed project structure
     - Main contains main OpenGL loop
     - VTGraphics contains graphic
     - Game contains gameSF and collision
     - Test contains Test cases
     - Model contains Datastructures and Types

--- src/Main.hs
+++ src/Main.hs
@@ -15,6 +15,7 @@ module Main where
 
 import Model
 import Game
+import VTGraphics
 import Graphics.UI.GLUT hiding (position, Position)
 import Data.IORef
 import FRP.Yampa
@@ -22,90 +23,6 @@ import FRP.Yampa.Utilities
 import Control.Arrow
 import Data.Time.Clock
 import Data.Maybe
-
--- TODO Rotation
--- Variablen: Accel-Value acc, Rationswinkel rot (0 ist in diesem Beispiel oben)
--- Formel y: acc * cos(rot*pi() / 180)
--- Formel x: acc * sin(rot*pi() / 180)
--- Roation sollte bei Druck auf KeyLeft / KeyRight einfach den Rotationswert modifizieren
-    
---TODO: Collision Angle:
--- distance :: Vector -> GLfloat
--- ColAng = arcsin ( y/distance)*180 / pi()
-
-{-
-distance :: Vector -> GLfloat
-distance (Vector x y) = sqrt(x*x + y*y)
--}
-        
-        {-}
-gameSF :: GameState -> SF Acceleration GameState
-gameSF [(GameObject posS velS accS Player),(GameObject posA velA accA Enemy)] = proc accShip -> do
-    rec
-     (posS,velS) <- accelerate posS velS -< accShip
-     (posA,velA) <- accelerate posA velA -< Vector (-0.01) (-0.01)
-     let (dps, dvs, dpa, dva) = collision (posS, velS, posA, velA)
-     --(ps', vs', pa', va') <- accumHoldBy (^+^) zeroVector -< Event (dps, dvs, dpa, dva) -- ersetzt mit den 4 unteren zeilen
-     ps' <- accumHoldBy (^+^) zeroVector -< Event dps
-     vs' <- accumHoldBy (^+^) zeroVector -< Event dvs
-     pa' <- accumHoldBy (^+^) zeroVector -< Event dpa
-     va' <- accumHoldBy (^+^) zeroVector -< Event dva
-    --returnA -< [GameObject ps' vs' accS Player, GameObject pa' va' accA Enemy] -- ersetzt mit unterer zeile
-    returnA -< [GameObject (ps' ^+^ posS) (vs' ^+^ velS) accS Player, GameObject (pa' ^+^ posA) (va' ^+^ velA) accA Enemy]
--}
-
-{-}
---Neue Kollisionsfunktion
---TODO: Geschwindigkeitsberechnung
-gameSF :: GameState -> SF Acceleration GameState
---gameSF [(GameObject posS velS accS Player),(GameObject posA velA accA Enemy)] = proc accShip -> do
-gameSF [o1, o2] = proc accShip -> do
-    rec
-        -- movement + collision player
-        velPreS <- iPre (vel o1) -< vS
-        colS <- collisionSF -< (posS', velPreS, posA', velPreA) -- keine SF mehr benötigt!!! nur noch collision die ein Event zurückgibt
-        --let colS = collision o1 o2
-        vS <- (vel o1 ^+^) ^<< impulseIntegral -< (accShip, colS)
-        posS' <- (pos o1 ^+^) ^<< integral -< vS
-        -- movement + collision enemy
-        velPreA <- iPre (vel o2) -< vA
-        colA <- collisionSF -< (posA', velPreA, posS', velPreS)
-        --let colA = collision o2 o1
-        vA <- (vel o2 ^+^) ^<< impulseIntegral -< (Vector (-0.01) (-0.01), colA)
-        posA' <- (pos o2 ^+^) ^<< integral -< vA
-        -- return new GameState
-    returnA -< [GameObject posS' vS (acc o1) Player, GameObject posA'  vA (acc o2) Enemy]
--}
-
-{- TODO: Hier müssen wir die korrekte neue Geschwindigkeit berechnen. 
-Dafür müssen wir Rotation, GameObjectMass, und GameObjectElas übergenen; 
-Die Größe wird bereits für die Kollisionserkennung verwendet
--}
-
-
-{-
-afterColVel :: Velocity -> Velocity -> Velocity
-afterColVel v1 v2 = (((-1) * elasS') *^ v1) ^+^ (((massA / massS) * 0.5) *^ v2)
-        where 
-         elasS' = elasS * (1 - 0) * 0.5
-         elasS = 1
-         massA = 1
-         massS = 1
-         -}
-{-
-VelS' = (ElasS' * VelS * (-1) + (0.5 - ElasS')) + (MassA `div` MassS) * VelA * 0.5
-        where ElasS' = ElasS * (1 - IsProjectile(A)) * 0.5
-        => Wie wir IsProjectile abfragen, müssen wir noch sehen. 
-           Die höchste Elastizität, die das System unterstützt, ist damit 50% (1.0)
--}
-    
-{-
-accelerate :: Position -> Velocity -> SF Acceleration (Position,Velocity)
-accelerate pos0 v0 = proc acc -> do
-    v <- (v0^+^) ^<< integral -< acc
-    pos <- (pos0^+^) ^<< integral -< v
-    returnA -<(pos,v)
--}
     
 -- set up OpenGL and start program
 main :: IO ()
@@ -136,7 +53,6 @@ createAWindow title = do
 
 
 ----------------- reactimate functions     ------------------
--- typed: ReactHandle Input Output -> ReactHandle () (Float,Float)
 
 -- init react
 initr :: IO Acceleration
@@ -155,6 +71,8 @@ display :: IO ()
 display = do
     return ()
     
+-- callback which is called, if openGL is in idle state.
+-- main signal function is called from this callback and game logic goes one tick further.
 idle :: IORef UTCTime -> IORef Action -> ReactHandle Acceleration GameState -> IO ()
 idle timeRef inputRef rh = do
     now <- getCurrentTime
@@ -165,20 +83,14 @@ idle timeRef inputRef rh = do
     react rh (realToFrac dt, Just (convAcc input))
     return ()
     
--- helper for converting input to acceleration
-convAcc :: Action -> Acceleration
-convAcc AccUp = Vector 0 1
-convAcc AccDown = Vector 0 (-1)
-convAcc AccLeft = Vector (-1) 0
-convAcc AccRight = Vector 1 0
-convAcc AccNone = Vector 0 0
-    
+-- callback which is called, if window gets resized. Handles the resizing process.
 resizeWindow :: Size -> IO ()
 resizeWindow size = do
     -- TODO keep aspect ratio
     -- viewport $= (Graphics.UI.GLUT.Position 0 0, size)
     return ()
 
+-- callback for handling keyboardMouse and mouse input
 keyboardMouse :: IORef Action -> KeyboardMouseCallback
 keyboardMouse inputRef (SpecialKey KeyLeft) Down _ _ = inputRef $= AccLeft
 keyboardMouse inputRef (SpecialKey KeyRight) Down _ _ = inputRef $= AccRight
@@ -187,150 +99,10 @@ keyboardMouse inputRef (SpecialKey KeyDown) Down _ _ = inputRef $= AccDown
 keyboardMouse inputRef _ Up _ _ = inputRef $= AccNone
 keyboardMouse _ _ _ _ _ = return ()
 
-{-}
-keyboardMouse inputRef key Down _ _ = case key of
-    (SpecialKey KeyLeft) -> inputRef $= AccLeft
-    (SpecialKey KeyRight) -> inputRef $= AccRight
-    (SpecialKey KeyUp) -> inputRef $= AccUp
-    (SpecialKey KeyDown) -> inputRef $= AccDown
-    _ -> return ()
-keyboardMouse inputRef key Up _ _ = inputRef $~! \_ -> AccNone
-keyboardMouse _ _ _ _ _ = return ()
--}
-   
-    
------------------ physics signal function ---------------------
-
-{-
--- free falling ball --
-fallingBall :: Pos -> Vel -> SF () (Pos, Vel)
-fallingBall y0 v0 = proc () -> do
-    v <- (v0+) ^<< integral -< (-9.81)
-    y <- (y0+) ^<< integral -< v
-    returnA -< (y,v)
-
--- detecting when ball goes through the floor --
-fallingBall' :: Pos -> Vel -> SF () ((Pos,Vel), Event (Pos,Vel))
-fallingBall' y0 v0 = proc () -> do
-    yv@(y,_) <- fallingBall y0 v0 -< ()
-    hit <- edge -< y <= 0
-    returnA -< (yv, hit `tag` yv)
-    
--- making the ball bounce --
-bouncingBall :: Pos -> SF () (Pos, Vel)
-bouncingBall y0 = bbAux y0 0.0
-    where
-        bbAux y0 v0 = switch (fallingBall' y0 v0) $ \(y,v) -> bbAux y (-v)
-        -}
-        
------------------- graphics ------------------------------------
-
-type Sprite = [Position] -- TODO rotation
-
-renderScene :: GameState -> IO ()
-renderScene [GameObject posS vS accS Player, GameObject posA _ _ Enemy] = do
-    clear [ColorBuffer]
-    loadIdentity
-    --mapM_ renderGameObject gs
-    renderPlayer posS
-    renderEnemy posA
-    --print (posS) -- ^-^ posA)
-    flush
-
-{-
----nur zum testen, solange SF nur SF () (Pos,Vel) ist und noch nicht GameState kann
-renderTest :: Pos -> IO ()
-renderTest pos = do
-    clear [ColorBuffer]
-    loadIdentity
-    renderPlayer pos
-    renderEnemy (0.5,0.3)
-    renderEnemy ((-0.4),(-0.3))
-    renderEnemy (0.8,(-0.2))
-    flush
--}    
-    
-renderGameObject :: GameObject -> IO ()
-renderGameObject (GameObject pos _ _ Player) = renderPlayer pos
-renderGameObject (GameObject pos _ _ Enemy) = renderEnemy pos
-
-renderPlayer :: Position -> IO ()
-renderPlayer pos = do
-    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (0::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo playerPointSet pos))
-    
-renderEnemy :: Position -> IO ()
-renderEnemy pos = do
-    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (1::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo enemyPointSet pos))
-    
-playerPointSet :: Sprite
-playerPointSet = let r=0.15 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
-
-enemyPointSet :: Sprite
-enemyPointSet = let r=0.1 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
-
-translateTo :: Sprite -> Position -> Sprite
-translateTo s pos = map (\pos' -> pos ^+^ pos') s
-
---convertPos :: Position -> GLPos
---convertPos (x,y) = (realToFrac x, realToFrac y)
-
-
-{-
--- represent a Position with OpenGL variables
-type GLPosition = (GLfloat, GLfloat)
--- represent a circle by a list of positions of point which form connected a circle
-type Circle = [GLPosition]
-
--- represent a circle by a set of points, their connection creates circle
-createCircle :: GLfloat -> Circle
-createCircle r = map (\t -> (r*cos (t), r*sin(t))) [0,0.2..(2*pi)]
-
--- represent player as circle
-player :: Circle
-player = createCircle 0.2
-
--- used to change the position of a Circle
-translateTo :: Circle -> Model.Position -> Circle
-translateTo c pos = let (x'', y'') = convertToGL pos in map (\(x',y') -> (x'+x'', y'+y'')) c
-
--- used to change the position of moveable circle
-translateTo' :: Circle -> (GLfloat,GLfloat) -> Circle
-translateTo' c (x,y) = map (\(x',y') -> (x'+x, y'+y)) c
-
--- converts "normal" haskell variables into OpenGL variables
-convertToGL :: Model.Position -> (GLfloat, GLfloat)
-convertToGL (Model.Position x y) = (realToFrac x, realToFrac y)
-    
--- draw moveable circle (=player)
-renderPlayer :: IORef (GLfloat,GLfloat) -> IO ()
-renderPlayer posRef = do
-        (x,y) <- readIORef posRef
-        repositionCamera (x,y)
-        renderPrimitive TriangleFan $ (color $ Color3 (1 :: GLfloat) (1 :: GLfloat) (1 :: GLfloat)) >> (mapM_ (\(x,y) -> vertex $ Vertex2 x y) (translateTo' player (x,y)))
-    
--- draw the ball
-renderBall :: Float -> IO ()
-renderBall posY = do
-    -- draw not moving circle
-    renderPrimitive TriangleFan $ (color $ Color3 (1 :: GLfloat) (1 :: GLfloat) (0 :: GLfloat)) >> (mapM_ (\(x,y) -> vertex $ Vertex2 x y) (translateTo player (Model.Position (-0.5) 0)))
-    --draw falling ball
-    renderPrimitive TriangleFan $ (color $ Color3 (1 :: GLfloat) (1 :: GLfloat) (0 :: GLfloat)) >> (mapM_ (\(x,y) -> vertex $ Vertex2 x y) (translateTo player (Model.Position 0 posY)))
-    
--- draw scene
-renderScene :: IORef (GLfloat, GLfloat) -> Float -> IO ()
-renderScene playerPos mBallPos = do
-    clear [ColorBuffer]
-    loadIdentity
-    renderPlayer playerPos
-    renderBall mBallPos
-    flush
-    
--- position camera based on key commands
-repositionCamera :: (GLfloat, GLfloat) -> IO ()
-repositionCamera (x,y) = do
-    let camPos = Vertex3 (realToFrac x) (realToFrac y) (1::GLdouble)
-    let camView = Vertex3 (realToFrac x) (realToFrac y) (0::GLdouble)
-    let upVec = Vector3 (0::GLdouble) (1::GLdouble) (0::GLdouble)
-    lookAt camPos camView upVec
-
--}
+-- helper for converting input to acceleration
+convAcc :: Action -> Acceleration
+convAcc AccUp = Vector 0 1
+convAcc AccDown = Vector 0 (-1)
+convAcc AccLeft = Vector (-1) 0
+convAcc AccRight = Vector 1 0
+convAcc AccNone = Vector 0 0
---
+++ src/VTGraphics.hs
@@ -0,0 +1,44 @@
+module VTGraphics where
+
+import Model
+import Graphics.UI.GLUT hiding (position, Position)
+import FRP.Yampa
+
+-- Sprite represent the graphic of an object, which is given by a set of Positions of Points
+type Sprite = [Position]
+
+-- render function to draw current GameState
+renderScene :: GameState -> IO ()
+renderScene [GameObject posS vS accS Player, GameObject posA _ _ Enemy] = do
+    clear [ColorBuffer]
+    loadIdentity
+    renderPlayer posS
+    renderEnemy posA
+    flush 
+
+-- render function to draw given GameObject
+renderGameObject :: GameObject -> IO ()
+renderGameObject (GameObject pos _ _ Player) = renderPlayer pos
+renderGameObject (GameObject pos _ _ Enemy) = renderEnemy pos
+
+-- render function to draw Player object
+renderPlayer :: Position -> IO ()
+renderPlayer pos = do
+    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (0::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo playerPointSet pos))
+
+-- render function to draw Enemey object    
+renderEnemy :: Position -> IO ()
+renderEnemy pos = do
+    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (1::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo enemyPointSet pos))
+
+-- Sprite representing the Players graphic -> Player is represented by a circle
+playerPointSet :: Sprite
+playerPointSet = let r=0.15 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
+
+-- Sprite representing the Enemies graphic -> Enemy is represented by a circle
+enemyPointSet :: Sprite
+enemyPointSet = let r=0.1 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
+
+-- function for translating a Sprite to a Position
+translateTo :: Sprite -> Position -> Sprite
+translateTo s pos = map (\pos' -> pos ^+^ pos') s
