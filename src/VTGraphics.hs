156aeeba19c9e7c1e56320708aa1d3a1adbfe163
Michael Schleier <michael-hermann.schleier@stud.uni-bamberg.de>
2016-08-24 14:12:00 +0200

    Added Triangle for PlayerGraphic

--- src/VTGraphics.hs
+++ src/VTGraphics.hs
@@ -18,26 +18,32 @@ renderScene gs = do
 -- render function to draw given GameObject
 -- third _ is rotation, 7th is size
 renderGameObject :: GameObject -> IO ()
-renderGameObject (GameObject pos _ _ _ _ _ _ _ _ _ Player) = renderPlayer pos
-renderGameObject (GameObject pos _ _ _ _ _ _ _ _ _ Enemy) = renderEnemy pos
+renderGameObject (GameObject pos _ _ rotation _ _ _ _ _ _ Player) = renderPlayer pos rotation
+renderGameObject (GameObject pos _ _ rotation _ _ _ _ _ _ Enemy) = renderEnemy pos
 
 -- render function to draw Player object
-renderPlayer :: Position -> IO ()
-renderPlayer pos = do
-    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (0::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo playerPointSet pos))
-
+renderPlayer :: Position -> Rotation -> IO ()
+renderPlayer pos rotation = do
+    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (0::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo playerCircleSet pos))
+    renderPrimitive TriangleFan $ (color $ Color3 (0::GLfloat) (1::GLfloat) (1::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo playerTriangleSet posTriangle))
+        where
+            posTriangle = pos ^+^ 0.15 *^ Vector (cos rotation) (sin rotation)
+    
 -- render function to draw Enemey object    
 renderEnemy :: Position -> IO ()
 renderEnemy pos = do
-    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (1::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo enemyPointSet pos))
+    renderPrimitive TriangleFan $ (color $ Color3 (1::GLfloat) (1::GLfloat) (0::GLfloat)) >> (mapM_ (\pos@(Vector x y) -> vertex $ Vertex2 x y) (translateTo enemyCircleSet pos))
 
 -- Sprite representing the Players graphic -> Player is represented by a circle
-playerPointSet :: Sprite
-playerPointSet = let r=0.15 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
+playerCircleSet :: Sprite
+playerCircleSet = let r=0.15 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
+
+playerTriangleSet :: Sprite
+playerTriangleSet = [Vector 0 0, Vector (-0.15) (-0.15), Vector 0.15 (-0.15)]
 
 -- Sprite representing the Enemies graphic -> Enemy is represented by a circle
-enemyPointSet :: Sprite
-enemyPointSet = let r=0.1 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
+enemyCircleSet :: Sprite
+enemyCircleSet = let r=0.1 in map (\t -> Vector (r*cos(t)) (r*sin(t))) [0,0.2..(2*pi)]
 
 -- function for translating a Sprite to a Position
 translateTo :: Sprite -> Position -> Sprite
