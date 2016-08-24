5fd0c5c9db2b7e770cc1fa0688bd3c3714b30728
UB-JM <jan.martin@stud.uni-bamberg.de>
2016-08-24 11:44:18 +0200

    GameObject derives Eq

--- src/Model.hs
+++ src/Model.hs
@@ -57,11 +57,8 @@ data GameObject = GameObject {
     hp :: Health,
     dmg :: Damage,
     objectType :: GameObjectType
-} deriving Show
-instance Eq GameObject where
-    GameObject pos1 vel1 acc1 rot1 spn1 mas1 ela1 size1 hp1 dmg1 type1 == GameObject pos2 vel2 acc2 rot2 spn2 mas2 ela2 size2 hp2 dmg2 type2
-        | pos1 == pos2 && vel1 == vel2 && acc1 == acc2 && rot1 == rot2 && spn1 == spn2 && mas1 == mas2 &&  ela1 == ela2 && size1 == size2 && hp1 == hp2 && dmg1 == dmg2 && type1 == type2 = True
-        | otherwise = False
+} deriving (Show, Eq)
+
 
 data GameObjectType = Player | Enemy | Asteroid | Projectile
     deriving (Eq, Show)
